e5d8e0437daadc3761db69b5c362522b3008bfba
krutovsky-danya <krutovsky.danya@gmail.com>
2022-01-05 19:58:38 +0500

    Input debug

--- Sokoban/Program.cs
+++ Sokoban/Program.cs
@@ -5,22 +5,18 @@ using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 class Player
 {
     static void Main(string[] args)
     {
-        string[] inputs;
-        inputs = Console.ReadLine().Split(' ');
+        var inputs = Console.ReadLine().Split(' ');
         int width = int.Parse(inputs[0]);
         int height = int.Parse(inputs[1]);
         int boxCount = int.Parse(inputs[2]);
         for (int i = 0; i < height; i++)
         {
             string line = Console.ReadLine();
+            Console.Error.WriteLine(line);
         }
 
         // game loop
@@ -35,9 +31,6 @@ class Player
                 int boxX = int.Parse(inputs[0]);
                 int boxY = int.Parse(inputs[1]);
             }
-
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
 
             Console.WriteLine("U");
         }
