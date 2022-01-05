3b2e354e5e11b75b3d17e3a8027bad2618350f3c
krutovsky-danya <krutovsky.danya@gmail.com>
2022-01-05 19:40:50 +0500

    Init

---
+++ Sokoban/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player
+{
+    static void Main(string[] args)
+    {
+        string[] inputs;
+        inputs = Console.ReadLine().Split(' ');
+        int width = int.Parse(inputs[0]);
+        int height = int.Parse(inputs[1]);
+        int boxCount = int.Parse(inputs[2]);
+        for (int i = 0; i < height; i++)
+        {
+            string line = Console.ReadLine();
+        }
+
+        // game loop
+        while (true)
+        {
+            inputs = Console.ReadLine().Split(' ');
+            int pusherX = int.Parse(inputs[0]);
+            int pusherY = int.Parse(inputs[1]);
+            for (int i = 0; i < boxCount; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int boxX = int.Parse(inputs[0]);
+                int boxY = int.Parse(inputs[1]);
+            }
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            Console.WriteLine("U");
+        }
+    }
+}
