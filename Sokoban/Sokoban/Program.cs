1d5caaf5140a609b2fbf0e7130eb35a03c8a3745
krutovsky-danya <krutovsky.danya@gmail.com>
2022-01-05 20:06:57 +0500

    Init

---
+++ Sokoban/Sokoban/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+class Player
+{
+    static void Main(string[] args)
+    {
+        string[] inputs;
+        inputs = Console.ReadLine().Split(' ');
+        int width = int.Parse(inputs[0]);
+        int height = int.Parse(inputs[1]);
+        int boxCount = int.Parse(inputs[2]);
+        for (int i = 0; i < height; i++)
+        {
+            string line = Console.ReadLine();
+            Console.Error.WriteLine(line);
+        }
+
+        // game loop
+        while (true)
+        {
+            inputs = Console.ReadLine().Split(' ');
+            int pusherX = int.Parse(inputs[0]);
+            int pusherY = int.Parse(inputs[1]);
+            for (int i = 0; i < boxCount; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int boxX = int.Parse(inputs[0]);
+                int boxY = int.Parse(inputs[1]);
+            }
+
+            Console.WriteLine("U");
+        }
+    }
+}
